99e4fea917c9c8b215dafc06c36a3575d1b9edff
karakib2k18 <karakib2k18@gmail.com>
2021-11-09 13:53:18 +0600

    localdate

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -43,7 +43,7 @@ const BookingModal = ({ openBooking, handleBookingClose, booking, date, setBooki
             date: date.toLocaleDateString()
         }
         // send to the server
-        fetch('http://localhost:5000/appointments', {
+        fetch('https://fathomless-sea-50814.herokuapp.com/appointments', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -13,7 +13,7 @@ const Appointments = ({ date }) => {
     const [appointments, setAppointments] = useState([])
 
     useEffect(() => {
-        const url = `http://localhost:5000/appointments?email=${user.email}&date=${date}`
+        const url = `https://fathomless-sea-50814.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
         fetch(url, {
             headers: {
                 'authorization': `Bearer ${token}`
