5375358294fc895b53fb2e4185e812de9dffab04
karakib2k18 <karakib2k18@gmail.com>
2021-11-18 17:52:44 +0600

    test

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -17,7 +17,7 @@ const AddDoctor = () => {
         formData.append('email', email);
         formData.append('image', image);
 
-        fetch('http://localhost:5000/doctors', {
+        fetch('https://fathomless-sea-50814.herokuapp.com/doctors', {
             method: 'POST',
             body: formData
         })
